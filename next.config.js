217b76cd8ff00491b6e25cbc97a85e6fa333b2c5
daleyu <yudale2003@gmail.com>
2023-03-03 21:40:29 -0500

    fix view error

--- next.config.js
+++ next.config.js
@@ -15,16 +15,6 @@ const nextConfig = {
         destination: "/home/pet",
         permanent: false,
       },
-      {
-        source: "/view/:",
-        destination: "/compose/:composerId/0",
-        permanent: false,
-      },
-      {
-        source: "/view/:recipientId",
-        destination: "/view/:recipientId/0",
-        permanent: false,
-      },
     ];
   },
 };
