a435862678242468cb11d3ca87856267587294ad
sciocaptain <67564019+sciocaptain@users.noreply.github.com>
2023-03-04 15:08:02 -0500

    Adding chatGPTAPI at 3:16PM 3/4/2023

---
+++ pages/api/chatGPTAPI.js
@@ -0,0 +1,27 @@
+const apiKey = "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";
+
+const sendMessageToChatGPT = async (message) => {
+  const response = await fetch(
+    "https://api.openai.com/v1/engines/davinci-codex/completions",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: JSON.stringify({
+        prompt: `The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.
+      User: ${message}
+      AI:`,
+      }),
+    }
+  );
+
+  const jsonResponse = await response.json();
+  const { choices } = jsonResponse;
+  const [{ text }] = choices;
+
+  return text;
+};
+
+export { sendMessageToChatGPT };
