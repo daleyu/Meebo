57a7b041b3213377a5795b8897abe1eb5e390b3a
Patrick <patrick.flanagan@stonybrook.edu>
2023-03-04 21:45:56 -0500

    animation added

--- pages/pet/[id]/index.tsx
+++ pages/pet/[id]/index.tsx
@@ -1,32 +1,57 @@
 /* eslint-disable jsx-a11y/alt-text */
-import Image from "next/image";
-import App from "../../../components/App";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import ChatWindow from "../../../components/ChatWindow";
 import { CenteringDiv, TextArea } from "../../../layouts/homePage.style";
 import {
   BootLegBody,
   OptionText,
   PetContainer,
   PetNameBubble,
-  QuestionText,
+  QuestionText
 } from "../../../layouts/pet.style";
-
+var counter = 0;
+var frequency = 1 / 45.0;
 const PetPage = ({ data, pageName }) => {
+  var [headWobble, setHeadWobble] = useState(0);
+  var amplitude = 0.01;
+  useEffect(() => {
+    const animationId = setInterval(() => {
+      setHeadWobble((prev) => prev + 0.05);
+    }, 10);
+    return () => clearInterval(animationId);
+  }, []);
+
   return (
     <div>
       <BootLegBody>
         <PetContainer>
-          <div style={{ display: "flex", justifyContent: "center" }}>
-            <Image
-              src={`/images/${pageName}_head.png`}
-              alt="your image"
-              width={500}
-              height={400}
-            />
-            <Image
+          <div>
+            <motion.img
               src={`/images/${pageName}_body.png`}
               alt="your image"
-              width={500}
-              height={400}
+              width={250}
+              height={200}
+              style={{
+                position: "absolute",
+                top: "5vw",
+                left: "21vw",
+              }}
+            />
+            <motion.img
+              src={`/images/${pageName}_head.png`}
+              alt="your image"
+              width={250}
+              height={200}
+              style={{
+                position: "absolute",
+                top: "6vw",
+                left: "21vw",
+              }}
+              animate={{
+                y: (headWobble =
+                  amplitude * headWobble * Math.sin(frequency * counter++)),
+              }}
             />
           </div>
         </PetContainer>
@@ -38,7 +63,7 @@ const PetPage = ({ data, pageName }) => {
           <OptionText>⚙️</OptionText>
         </CenteringDiv>
         <CenteringDiv>
-          <App />
+          <ChatWindow />
         </CenteringDiv>
       </BootLegBody>
     </div>
