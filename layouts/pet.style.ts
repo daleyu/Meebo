84ba0b88d7081c741804f82bb6659e5d189f0835
Moinak Das <52611169+Sadkoi@users.noreply.github.com>
2023-03-04 20:51:12 -0500

    page 2 start

--- layouts/pet.style.ts
+++ layouts/pet.style.ts
@@ -3,9 +3,9 @@ import styled from "styled-components";
 //make a new styled component called PetContainer that wraps image in circle
 export const PetContainer = styled.div`
   overflow: hidden;
-  width: 30%;
+  width: 28%;
   margin: 0 auto;
-  margin-top: 15vw;
+  margin-top: 12vw;
   margin-bottom: 20px;
 `;
 export const PetContainers = styled.div`
@@ -29,19 +29,37 @@ export const PetName = styled.h1`
   margin-bottom: 10px;
 `;
 
-export const PetRowContainer = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 20px;
-  margin-top: 50px;
-  margin-bottom: 50px;
-`;
-
 export const BootLegBody = styled.div`
   background: linear-gradient(106.07deg, #a9dcc5 8.55%, #60a888 93.3%);
   width: 100%;
-  height: 100vw;
+  height: 100vh;
   overflow-x: hidden;
+  overflow-y: hidden;
+`;
+
+export const GearL = styled.div`
+  width: 50vw;
+  position: absolute;
+  top: 0px;
+  left: -32vw;
+  overflow: hidden;
+`;
+
+export const GearR = styled.div`
+  width: 50vw;
+  position: absolute;
+  top: 0px;
+  left: 82vw;
+  overflow: hidden;
+`;
+
+export const Description = styled.div`
+  width: 100vw;
+  position: absolute;
+  top: 40vw;
+  left: 0vw;
+  overflow: hidden;
+  font-size: 1.5vw;
+  font-family: heebo;
+  text-align: center;
 `;
